cb3c7a573b09003bff45071437b60d5606669ef1
ankit bathla <ankit@qikstay.com>
2017-07-26 18:16:33 +0530

    two way binding

--- app.js
+++ app.js
@@ -4,10 +4,8 @@ app.controller("emp", ["$scope", function($scope) {
     $scope.a = 10;
     $scope.b = 20;
 
-    $scope.doDoubleAndSum = function() {
-        $scope.a = $scope.a * 2;
-        $scope.b = $scope.b * 2;
-        $scope.sum = parseInt($scope.a) + parseInt($scope.b);
 
+    $scope.doSum = function() {
+        $scope.sum = parseInt($scope.a) + parseInt($scope.b);
     };
 }])
