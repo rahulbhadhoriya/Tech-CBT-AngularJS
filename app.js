f825ccaeb7f6b47555e4929fc3fa2f9d4127211e
ankit bathla <ankit@qikstay.com>
2017-07-26 17:56:31 +0530

    one time binding

---
+++ app.js
@@ -0,0 +1,13 @@
+// JavaScript source code
+var app = angular.module("app", []);
+app.controller("emp", ["$scope", function($scope) {
+    $scope.a = 10;
+    $scope.b = 20;
+
+    $scope.doDoubleAndSum = function() {
+        $scope.a = $scope.a * 2;
+        $scope.b = $scope.b * 2;
+        $scope.sum = parseInt($scope.a) + parseInt($scope.b);
+
+    };
+}])
