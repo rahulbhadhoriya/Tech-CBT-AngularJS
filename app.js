74b44dee9657ee03ea608b76b6814980f217f9b3
Rahul Bhadhoriya <ankit@qikstay.com>
2017-07-28 16:45:40 +0530

    a synchronous call with call back function

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
 // JavaScript source code
 var app = angular.module("app", []);
-app.controller("emp", ["$scope", function($scope) {
+app.controller("emp", ["$scope", "calcFactory", function($scope, calcFactory) {
     $scope.a = 10;
     $scope.b = 20;
 
 
     $scope.doSum = function() {
-        $scope.sum = parseInt($scope.a) + parseInt($scope.b);
+        // $scope.sum = calcFactory.getSum($scope.a, $scope.b);
+        calcFactory.getSum($scope.a, $scope.b, function(r) {
+            $scope.sum = r;
+        });
+
     };
+}]);
+app.factory("calcFactory", ["$http", "$log", function($http, $log) {
+    $log.log("instantiating calcFactory...");
+    var oCalcService = {};
+    /* oCalcService.getSum = function(a, b) {
+         return parseInt(a) + parseInt(b);
+
+     };*/
+    oCalcService.getSum = function(a, b, cb) {
+        var s = parseInt(a) + parseInt(b);
+        cb(s);
+    };
+    return oCalcService;
 }])
